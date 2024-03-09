ff2cf03c72e7f09906675d1ad1d625c7eedbebc7
Tomasz Boboli <tboboli@gmali.com>
2024-03-09 22:05:50 +0100

    Drawing based on a table

--- main.js
+++ main.js
@@ -2,50 +2,52 @@ const fs = require('node:fs');
 
 const NR_OF_TILES_PER_ROW = 4
 const NR_OF_ROWS = 1
+var content
 
+points = [[0,0]	,	[-0.866, 0.5],	[-0.866, 1.5]	,	[0,2]	,	[0.866,1.5]	,	[0.866,0.5]];
 
 function add(txt) {
   content += txt + "\n";
 }
 
-let content = "";
-content += "M3 S0\n";
-content += "\n";
+function main() {
 
-for(let row = 0; row < NR_OF_ROWS; row++) {
-  for(let col = 0; col < NR_OF_TILES_PER_ROW; col++) {
+  content = "";
+  add("M3 S0");
+  add("");
 
-    content += "S0\n";
+  for(let row = 0; row < NR_OF_ROWS; row++) {
+    for(let col = 0; col < NR_OF_TILES_PER_ROW; col++) {
 
-    // content += "G0X30.736Y47.641\n";
-    // content += "S1000\n";
+      add("S0");
 
-    // content += "G1X46.57Y39.175F1000\n";
-    // content += "Y22.243\n";
-    // content += "X30.736Y13.777\n";
-    // content += "X14.902Y22.243\n";
-    // content += "X14.902Y39.175\n";
-    // content += "X30.736Y47.641\n";
-    add("G0X0Y0");
-    add("S1000");
-    add("G1X-0.866Y0.5F1000");
-    add("Y1.5");
-    add("X0Y2");
-    add("G1X0.866Y1.5F1000");
-    add("Y0.5");
-    add("X0Y0");
+      // go to the first point
+      add(`G0X${points[0][0]}Y${points[0][1]}`);
+      add("S1000");    
+      
+      // cut to the first point
+      add(`G0X${points[1][0]}Y${points[1][1]}F1000`);
 
-    content += "\n";
+      // continue cutting to further points
+      for(i = 2; i < points.length; i++)
+        add(`X${points[i][0]}Y${points[i][1]}F1000`);
+      
+      // cut back to the last point
+      add(`X${points[0][0]}Y${points[0][1]}`);
 
+      add("");
+
+    }
   }
+
+  add("S0");
+  add("M5 S0");
+
+  fs.writeFile('test.nc', content, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
 }
-
-
-
-fs.writeFile('test.nc', content, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    // file written successfully
-  }
-});
